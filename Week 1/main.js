78fea2bbf9a7f86d9d3a5ab6b960bc28d4d34b53
mleendeakin <87801139+mleendeakin@users.noreply.github.com>
2021-09-10 20:00:18 +1000

    Add files via upload

---
+++ Week 1/main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    el:"#myForm",
+    data() {
+        return { 
+           text1: "Assesment 1",
+           text2: "Assesment 2",
+           text3: "Assesment 3",
+                   
+        }
+    },
+})
+
+app.mount("#app")
